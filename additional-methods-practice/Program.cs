842fa0aae9ae7511e9fe76f650700c853a1502fb
Thad <thadisfree@gmail.com>
2021-10-20 10:24:19 -0500

    output still not working...

---
+++ additional-methods-practice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace additional_methods_practice
+{
+    class Program
+    {
+        //10-20-2021 not working, won't give output but no errors detected
+        public static string NewMethod(string p)
+        {
+           return p.ToLower();
+
+            
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"This method returns any string to all lower case characters..." +
+                $"Please type in a string and see the results...thanks");
+
+            NewMethod(Console.ReadLine());
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
